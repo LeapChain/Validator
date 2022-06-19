1d23c2016588aa90313aafae627fa148290c98af
mrbusysky <58412572+mrbusysky@users.noreply.github.com>
2022-06-19 16:20:37 -0700

    Edit locked 1
    This edit was to see why the balances for locked were not pulling in the database.

--- v1/accounts/factories/account.py
+++ v1/accounts/factories/account.py
@@ -9,7 +9,7 @@ class AccountFactory(DjangoModelFactory):
     account_number = Faker('pystr', max_chars=VERIFY_KEY_LENGTH)
     balance = Faker('pyint', max_value=MAX_POINT_VALUE)
     balance_lock = Faker('pystr', max_chars=BALANCE_LOCK_LENGTH)
-    locked = Faker('pystr', max_chars=VERIFY_KEY_LENGTH)
+    locked = Faker('pystr', max_chars=MAX_POINT_VALUE)
 
     class Meta:
         model = Account
